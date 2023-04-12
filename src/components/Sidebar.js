6d69b161cbdb8223a090ccb7041586f66ccf834d
skerrey <sethkerrey@gmail.com>
2023-04-12 17:15:37 -0500

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,11 +5,70 @@ import './Sidebar.scss'
 function Sidebar() {
   return (
     <div className="sidebar">
-      <NavLink to="/" className="link" activeClassName="active">Home</NavLink>
-      <NavLink to="/payments" className="link" activeClassName="active">Payments</NavLink>
-      <NavLink to="/maintenance" className="link">Maintenance</NavLink>
-      <NavLink to="/contact-us" className="link">Contact Us</NavLink>
-      <NavLink to="/settings" className="link">Settings</NavLink>
+      <div class="sidebar-title">
+        <div className="welcome-message">Hello</div>
+        <div className="username">John Doe</div>
+      </div>
+      <NavLink 
+        to="/" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#0d6efd" : "transparent"
+        })}
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/payments" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#20c997" : "transparent"
+        })}
+      >
+        Payments
+        </NavLink>
+      <NavLink 
+        to="/maintenance" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#fd7e14" : "transparent"
+        })}
+      >
+        Maintenance
+      </NavLink>
+      <NavLink 
+        to="/contact-us" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#0dcaf0" : "transparent"
+        })}
+      >
+        Contact Us
+      </NavLink>
+      <NavLink 
+        to="/account-profile" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#0d6efd" : "transparent"
+        })}
+      >
+        Account Profile
+      </NavLink>
+      <NavLink 
+        to="/settings" 
+        className="link"
+        activeClassName="active"
+        style={isActive => ({
+          borderLeftColor: isActive ? "#6f42c1" : "transparent"
+        })}
+      >
+        Settings
+      </NavLink>
     </div>
   )
 }
