b2c3e22915e40d83ad4d49f9234414cb6465e390
skerrey <sethkerrey@gmail.com>
2023-04-12 18:39:41 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Header.scss'
+import logo from "../images/logo3.svg"
 
 function Header() {
   return (
     <div className="header">
+      <img src={logo} alt="logo" className="logo" />
       Header
     </div>
   )
