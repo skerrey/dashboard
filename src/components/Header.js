a4d14d65a3bd6f4e37823d52e252ce0e6bb64c2f
skerrey <sethkerrey@gmail.com>
2023-04-12 19:02:09 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,14 @@ import logo from "../images/logo.svg"
 function Header() {
   return (
     <div className="header">
+      <div>
       <img src={logo} alt="logo" className="logo" />
-      Header
+      </div>
+      <div>
+        <span className="pre-address">Property address</span>
+        <span className="address">123 Main St, Anytown, USA</span>
+        <span className="logout">Log Out</span>
+      </div>
     </div>
   )
 }
