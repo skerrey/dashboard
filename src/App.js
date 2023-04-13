e5d34795fc79ef4e0a20d690b49919b8a4d8210c
skerrey <sethkerrey@gmail.com>
2023-04-12 20:37:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+// Description: App.js
 
-
+// Routes
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
+
+// Components
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -9,11 +13,20 @@ import Maintenance from './components/Maintenance';
 import ContactUs from './components/ContactUs';
 import AccountProfile from "./components/AccountProfile";
 import Settings from './components/Settings';
+
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+
+// Styling
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.scss';
+import "./helpers/FontAwesomeIcons";
 
 function App() {
   return (
     <>
+      <AuthProvider>
       <Router>
         <Sidebar />
         <Header />
@@ -26,10 +39,14 @@ function App() {
             <Route path="/contact-us" element={<ContactUs />} />
             <Route path="/account-profile" element={<AccountProfile />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
         </div>
         
       </Router> 
+      </AuthProvider>
     </>
   );
 }
