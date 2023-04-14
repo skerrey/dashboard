36bd9ef2bdea7af82861a2b369d855681e0b2f53
skerrey <sethkerrey@gmail.com>
2023-04-14 13:12:06 -0500

    Fix Routes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,59 @@
+// Working with links
+
 // Description: App.js
 
 // Routes
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import AuthProvider, { useAuth } from "./contexts/AuthContext";
+import PrivateRoutes from "./routes/PrivateRoutes";
+
+// Pages
+import Home from './pages/Home';
+import Payments from './pages/Payments';
+import Maintenance from './pages/Maintenance';
+import ContactUs from './pages/ContactUs';
+import AccountProfile from "./pages/AccountProfile";
+import Settings from './pages/Settings';
 
 // Components
-import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import Home from './components/Home';
-import Payments from './components/Payments';
-import Maintenance from './components/Maintenance';
-import ContactUs from './components/ContactUs';
-import AccountProfile from "./components/AccountProfile";
-import Settings from './components/Settings';
-
+import Sidebar from './components/Sidebar';
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
 
+// Layouts
+import PrivateLayout from "./layouts/PrivateLayout";
+import GuestLayout from "./layouts/GuestLayout";
+
 // Styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.scss';
-import "./helpers/FontAwesomeIcons";
+import "./utils/FontAwesomeIcons";
+import { Fragment } from "react";
 
 function App() {
   return (
     <>
       <AuthProvider>
-      <Router>
-        <Sidebar />
-        <Header />
-
-        <div className="main">
+        <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/payments" element={<Payments />} />
-            <Route path="/maintenance" element={<Maintenance />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/account-profile" element={<AccountProfile />} />
-            <Route path="/settings" element={<Settings />} />
+
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
+
+
+            <Route element={<PrivateLayout />}>
+              <Route path="/" element={<PrivateRoutes><Home /></PrivateRoutes>} />
+              <Route path="/payments" element={<PrivateRoutes><Payments /></PrivateRoutes>} />
+              <Route path="/maintenance" element={<PrivateRoutes><Maintenance /></PrivateRoutes>} />
+              <Route path="/contact-us" element={<PrivateRoutes><ContactUs /></PrivateRoutes>} />
+              <Route path="/account-profile" element={<PrivateRoutes><AccountProfile /></PrivateRoutes>} />
+              <Route path="/settings" element={<PrivateRoutes><Settings /></PrivateRoutes>} />
+            </Route>
           </Routes>
-        </div>
-        
-      </Router> 
+        </Router> 
       </AuthProvider>
     </>
   );
