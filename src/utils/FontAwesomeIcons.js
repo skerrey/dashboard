b07f3a2928177ab9c9553be0a76f9b2b1c2d69b2
skerrey <sethkerrey@gmail.com>
2023-04-15 20:16:19 -0500

    Create FontAwesomeIcons.js

---
+++ src/utils/FontAwesomeIcons.js
@@ -0,0 +1,12 @@
+// Description: Font Awesome Icons library 
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { 
+  faRightFromBracket,
+  faHouse 
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faRightFromBracket,
+  faHouse
+);
