8e7c9ed0d14345304e842a12bf98679f55b84fed
skerrey <sethkerrey@gmail.com>
2023-04-16 14:18:42 -0500

    Update PublicRoutes.js

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-export default function PrivateRoute({ children }) {
+export default function PublicRoutes({ children }) {
   const { currentUser } = useAuth();
   
   return (
