7f7e6fb0b92653cf3803ec6bfe2dd5a547ee821a
skerrey <sethkerrey@gmail.com>
2023-04-15 20:16:15 -0500

    Create PublicRoutes.js

---
+++ src/routes/PublicRoutes.js
@@ -0,0 +1,14 @@
+// Description: Public Routes for non-authenticated users
+
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function PrivateRoute({ children }) {
+  const { currentUser } = useAuth();
+  
+  return (
+    !currentUser ? children : <Navigate to="/" redirect />
+  )
+}
+
