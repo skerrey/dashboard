00e5885534afa5a482b6d74ecee69588053c9493
skerrey <sethkerrey@gmail.com>
2023-04-19 14:33:56 -0500

    Update PrivateLayout.js

--- src/layouts/PrivateLayout.js
+++ src/layouts/PrivateLayout.js
@@ -3,14 +3,42 @@
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
+import { useSidebar } from '../utils/Responsive';
 
 function PrivateLayout() {
+  const { isSidebarOpen, toggleSidebar, isMobile } = useSidebar();
+
+  const handleClose = () => {
+    if (isSidebarOpen && isMobile) {
+      toggleSidebar();
+    }
+  }
+
+  const TransparentBackground = () => {
+    const style = {
+      display: isSidebarOpen && isMobile ? "block" : "none",
+      backgroundColor: isSidebarOpen && isMobile ? "rgba(255, 255, 255, 0.6)" : "transparent",
+      zIndex: "2",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+    }
+  
+    return (
+      <span style={style} onClick={handleClose} />
+    );
+  }
+
   return (
     <>
-      <Sidebar />
+      <TransparentBackground />
+
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
       <Header />
       <div className="private-layout">
-        <Outlet />
+        <Outlet  />
       </div>
     </>
   );
