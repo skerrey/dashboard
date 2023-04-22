06b175808591ad33dc7f1681cd93ebb82c977ccc
skerrey <sethkerrey@gmail.com>
2023-04-22 13:54:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './index.scss';
 import App from './App';
 import AuthProvider from './contexts/AuthContext';
