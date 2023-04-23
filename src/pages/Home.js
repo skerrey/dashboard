0e12b27b2f8d10cc5ff5453ab257e2dc5d2944ff
skerrey <sethkerrey@gmail.com>
2023-04-23 18:56:02 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,64 @@
 // Description: Home page
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { Row, Col, Card } from 'react-bootstrap';
 import './Home.scss';
 
 function Main() {
-  const { currentUser } = useAuth();
-
 
   return (
     <div>
-      Main
-      { currentUser ? 
-        <div>Logged in as {currentUser.email}</div> 
-        : 
-        <div>Not logged in</div> 
-        
-      }
+      <div className="page-title">
+        Home
+      </div>
+      <Row md={1} lg={2}>
+        <Col>
+          <Card className="card-payments">
+            <Card.Body>
+              <Card.Title>Payments</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card className="card-maintenance">
+            <Card.Body>
+              <Card.Title>Maintenance</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row md={1} lg={2}>
+        <Col>
+          <Card className="card-account">
+            <Card.Body>
+              <Card.Title>Account Profile</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+        <Card className="card-settings">
+            <Card.Body>
+              <Card.Title>Settings</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
     </div>
   )
