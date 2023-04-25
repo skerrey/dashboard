6928e19a6b96704ef4ca9fb4c214c34d613c9c4c
skerrey <sethkerrey@gmail.com>
2023-04-25 18:21:13 -0500

    Add icons

--- src/utils/FontAwesomeIcons.js
+++ src/utils/FontAwesomeIcons.js
@@ -4,11 +4,21 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { 
   faRightFromBracket,
   faHouse,
-  faBars
+  faBars,
+  faCircleInfo,
+  faBuildingColumns,
+  faScrewdriverWrench,
+  faUser,
+  faGear
 } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
   faRightFromBracket,
   faHouse,
-  faBars
+  faBars,
+  faCircleInfo,
+  faBuildingColumns,
+  faScrewdriverWrench,
+  faUser,
+  faGear
 );
