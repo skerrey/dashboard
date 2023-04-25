4b7d701692c775e95619573c1f781e0f3028d51c
skerrey <sethkerrey@gmail.com>
2023-04-25 18:21:21 -0500

    Update Settings.js

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,16 @@
 // Description: Settings page
 
-import React from 'react'
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Settings() {
   return (
-    <div>Settings</div>
+    <div>
+      <div className="page-title">
+        Settings <FontAwesomeIcon icon="fa-solid fa-gear" size="xs" />
+      </div>
+    </div>
   )
 }
 
