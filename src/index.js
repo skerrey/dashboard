ca6f56e613d34325c0dc1dfa81a5e3bc67334681
skerrey <sethkerrey@gmail.com>
2023-04-25 18:21:54 -0500

    Removed StrictMode

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import AuthProvider from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
 
-  </React.StrictMode>
+  // </React.StrictMode>
 );
