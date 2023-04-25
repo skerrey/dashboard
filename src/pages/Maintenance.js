a166b6acfa9ef3071f30072159ef1fef4cb9802e
skerrey <sethkerrey@gmail.com>
2023-04-25 18:21:25 -0500

    Update Maintenance.js

--- src/pages/Maintenance.js
+++ src/pages/Maintenance.js
@@ -1,10 +1,16 @@
 // Description: Maintenance page
 
-import React from 'react'
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Maintenance() {
   return (
-    <div>Maintenance</div>
+    <div>
+      <div className="page-title">
+        Maintenance <FontAwesomeIcon icon="fa-solid fa-screwdriver-wrench" size="xs" />
+      </div>
+    </div>
   )
 }
 
