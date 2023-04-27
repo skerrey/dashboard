b5d9ecb4a1b0a819a3d6d3e7088445c6fb971fb2
skerrey <sethkerrey@gmail.com>
2023-04-27 12:01:50 -0500

    Create StorageContext.js

---
+++ src/contexts/StorageContext.js
@@ -0,0 +1,44 @@
+// Description: Authentication Context for Firebase
+
+import React, { useContext } from 'react';
+import { storage} from '../firebase.config';
+import { uploadBytesResumable, ref } from 'firebase/storage';
+
+const StorageContext = React.createContext();
+
+export function useStorage() {
+  return useContext(StorageContext);
+};
+
+export default function StorageProvider({ children }) {
+
+  // Upload files to Firebase Storage 
+  const uploadFiles = (fileArray, maintenanceId, userId) => {
+    fileArray.forEach((file) => {
+      const storageRef = ref(storage, `/${userId}/maintenance/${maintenanceId}/maintenance-images/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          console.log('Upload is complete');
+        }
+      );
+    })
+  };
+
+  const value = {
+    uploadFiles,
+  };
+
+  return (
+    <StorageContext.Provider value={value}>
+      {children}
+    </StorageContext.Provider>
+  );
+}
