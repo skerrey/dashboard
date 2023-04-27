8f1da6ca0cb13b61bcd36f8c9d8f676c0299c36a
skerrey <sethkerrey@gmail.com>
2023-04-27 12:01:47 -0500

    Add Contexts

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.scss';
 import App from './App';
 import AuthProvider from './contexts/AuthContext';
+import FirestoreProvider from './contexts/FirestoreContext';
+import StorageProvider from './contexts/StorageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <AuthProvider>
+      <FirestoreProvider>
+      <StorageProvider>
       <App />
+      </StorageProvider>
+      </FirestoreProvider>
     </AuthProvider>
 
   // </React.StrictMode>
