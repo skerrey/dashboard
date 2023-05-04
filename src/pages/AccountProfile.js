0880b26dd830e8554d8e08d8b7a70ec403f8a5bc
skerrey <sethkerrey@gmail.com>
2023-05-03 21:24:32 -0500

    Update AccountProfile.js

--- src/pages/AccountProfile.js
+++ src/pages/AccountProfile.js
@@ -1,14 +1,50 @@
 // Description: Account Profile page
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { httpsCallable } from "firebase/functions";
+import { functions } from '../firebase.config';
+
 function AccountProfile() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    const getUsers = httpsCallable(functions, 'getUsers');
+    getUsers()
+      .then((result) => {
+        console.log(result.data);
+        setUsers(result.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        console.log("whoops");
+      });
+  };
+
+  useEffect(() => {
+    console.log("fetching users...");
+    fetchUsers();
+  }, []);
+
+  console.log(users);
+
   return (
     <div>
       <div className="page-title">
         Account Profile <FontAwesomeIcon icon="fa-solid fa-user" size="xs" />
+      </div>
+      <div>
+      {users.length > 0 ? (
+        <ul>
+        {users.map((user) => (
+          <li key={user.uid}>{user.email}</li>
+        ))}
+        </ul>
+      ) : (
+        <li>No users</li>
+      )}
       </div>
     </div>
   )
