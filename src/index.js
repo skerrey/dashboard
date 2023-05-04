f4feb51b0e20836a3851be57dba350efd48d65c8
skerrey <sethkerrey@gmail.com>
2023-05-03 21:24:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,14 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.scss';
 import App from './App';
-import AuthProvider from './contexts/AuthContext';
-import FirestoreProvider from './contexts/FirestoreContext';
-import StorageProvider from './contexts/StorageContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <AuthProvider>
-      <FirestoreProvider>
-      <StorageProvider>
+    <>
       <App />
-      </StorageProvider>
-      </FirestoreProvider>
-    </AuthProvider>
+    </>
 
   // </React.StrictMode>
 );
