8db974c7da8c39cef824119d4058309a7194c249
skerrey <sethkerrey@gmail.com>
2023-05-09 20:10:34 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
-const functions = require("firebase-functions");
+/* eslint-disable max-len */
+// Description: This file is used to deploy firebase functions
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+const cors = require("cors")();
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Get all users
+exports.getUsers = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    // List batch of users, 1000 at a time.
+    // eslint-disable-next-line require-jsdoc
+    const listUsers = (nextPageToken) => {
+      admin.auth().listUsers(1000, nextPageToken)
+        .then((listUsersResult) => {
+          const users = listUsersResult.users.map((user) => Object.assign({}, user.toJSON()));
+          if (listUsersResult.pageToken) {
+            // List next batch of users.
+            listUsers(listUsersResult.pageToken);
+          } else {
+            res.setHeader("Content-Type", "application/json");
+            res.send({
+              "status": "success",
+              "message": "Users fetched successfully",
+              "data": users,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log("Error fetching users: ", error);
+          res.status(500).send("Error fetching users");
+        });
+      };
+      listUsers();
+  });
+});
