8aa6a5cadeb0ed5609089e6811695bf34e382aea
skerrey <sethkerrey@gmail.com>
2023-05-18 19:18:09 -0500

    Update for address

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 // Description: Header component
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.scss';
 import logo from "../images/logo.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { useFirestore } from '../contexts/FirestoreContext';
 
 function Header() {
   const [error, setError] = useState('');
-  const { logout } = useAuth();
+  const [address, setAddress] = useState(''); 
+  const { logout, userId } = useAuth();
+  const { getAddress } = useFirestore();
   const navigate = useNavigate();
 
   async function handleLogout() { // logout user on click
@@ -24,6 +27,17 @@ function Header() {
     }
   }
 
+  // Get user phone from Firestore
+  useEffect(() => {
+    getAddress(userId)
+      .then((address) => {
+        setAddress(address);
+      })
+      .catch((error) => {
+        console.log("Error getting user phone:", error);
+      });
+  }, [getAddress, userId]);
+
   return (
     <div className="header">
       <div>
@@ -31,7 +45,14 @@ function Header() {
       </div>
       <div>
         <span className="address-title">Property address</span>
-        <span className="address">123 Main St, Anytown, USA</span>
+        <span className="address">
+          {address && address.address2 ?
+            `${address.address} ${address.address2}, ${address.city}, ${address.state} ${address.zip}`  
+            : address 
+            ? `${address.address}, ${address.city}, ${address.state} ${address.zip}`
+            :  '(no address listed)' // default address
+          }
+        </span>
         <button className="btn btn-logout" onClick={handleLogout}>
           <FontAwesomeIcon icon="fa-solid fa-right-from-bracket" size="sm" /> Log Out
         </button>
