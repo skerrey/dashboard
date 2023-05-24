f8714dd5ed7ede5eec4a35fb24ca51a8fbe2eeb0
skerrey <sethkerrey@gmail.com>
2023-05-24 15:19:02 -0500

    Update Payments.js

--- src/pages/Payments.js
+++ src/pages/Payments.js
@@ -1,121 +1,101 @@
 // Description: Payments page
 
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { Row, Col, Card, Button, InputGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { httpsCallable } from "firebase/functions";
-import { functions } from '../firebase.config';
 import { loadStripe } from "@stripe/stripe-js";
-import {
-  Elements,
-  PaymentElement,
-  useStripe,
-  useElements
-} from "@stripe/react-stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useAuth } from '../contexts/AuthContext';
+import CardPaymentForm from '../components/CardPaymentForm';
+import AddCardForm from '../components/AddCardForm';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
-const createPayment = async (data) => {
-  const createPaymentIntent = httpsCallable(functions, 'createPaymentIntent');
-  return createPaymentIntent(data);
-};
-
-function PaymentForm({ onSubmit }) {
-  const [amount, setAmount] = useState('');
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    onSubmit(amount);
-  };
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>Amount</Form.Label>
-        <Form.Control 
-          type="number" 
-          placeholder="Enter amount" 
-          value={amount}
-          onChange={e => setAmount(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-}
-
-// This component displays the card input field
-function CardInput({ clientSecret }) {
-  const stripe = useStripe();
-  const elements = useElements();
-
-  const paymentElementOptions = { layout: "tabs" }
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (!stripe || !elements) {
-      return;
-    }
-
-    const { paymentMethod, error } = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: elements.getElement(PaymentElement),
-      },
-    });
-
-    if (error) {
-      console.log('[error]', error);
-    } else {
-      if (paymentMethod) {
-        console.log('[PaymentMethod]', paymentMethod);
-        // TODO: send the paymentMethod.id to your server
-      }
-    }
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement id="payment-element" className="pb-3" options={paymentElementOptions} />
-      <button type="submit" disabled={!stripe}>Pay Rent</button>
-    </form>
-  );
-}
-
 function Payments() {
-  const [clientSecret, setClientSecret] = useState(null);
+  const { currentUser } = useAuth();
+  const [clientSecret, setClientSecret] = useState("");
+  const [amount, setAmount] = useState(1000);
 
-  const handleSubmit = amount => {
-    createPayment({ amount: Number(amount) })
-      .then(result => {
-        setClientSecret(result.data.clientSecret);
-      })
-      .catch(error => {
-        console.error('Error creating payment intent:', error);
-        // Handle error, e.g. by displaying an error message to the user
-      });
+  const handleAmountChange = (event) => {
+    const amountInCents = event.target.value * 100;
+    setAmount(amountInCents);
   };
 
-  if (!clientSecret) {
-    // return <PaymentForm onSubmit={handleSubmit} />;
-    return <div>Coming Soon!</div>
-  }
+  const createCustomer = async () => {
+    try {
+      const response = await fetch("https://us-central1-dashboard-c48b3.cloudfunctions.net/createStripeCustomer", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user: currentUser }),
+      });
+
+      const data = await response.json();
+      if (data.success) {
+        console.log('Stripe customer created successfully');
+      } else {
+        console.log('Error creating Stripe customer');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://us-central1-dashboard-c48b3.cloudfunctions.net/createPaymentIntent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ amount: amount }),
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return res.text().then(text => {throw new Error(text)});
+      }
+      return res.json();
+    })
+    .then((data) => setClientSecret(data.clientSecret))
+    .catch((error) => console.error('Error:', error));
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+
+  const options = {
+    clientSecret,
+    appearance,
+  };
 
   return (
     <div>
       <div className="page-title">
         Payments <FontAwesomeIcon icon="fa-solid fa-building-columns" size="xs" />
       </div>
-      <Row>
+      <Row xs={1} sm={1} md={1} lg={2}>
         <Col className="col">
           <Card className="card-payments">
             <Card.Body>
               <Card.Title>Pay Rent</Card.Title>
-              <Elements stripe={stripePromise}>
-                <CardInput clientSecret={clientSecret} />
-              </Elements>
+              Set up a payment method to pay rent.
+              <hr className="text-muted" />
+
+              <Button onClick={createCustomer}>Create Payment</Button>
+              <input type="number" min="1" step="any" onChange={handleAmountChange} value={amount / 100} placeholder="Enter amount in dollars" />
+
+              {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
+                  <AddCardForm />
+                </Elements>
+              )}
+
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="col">
+          <Card className="card-payments">
+            <Card.Body>
+              <Card.Title>Payment History</Card.Title>
+              <hr className="text-muted" />
+
             </Card.Body>
           </Card>
         </Col>
