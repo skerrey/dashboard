b3e0e28e694e84e7ae07eae401c944a3f952ac79
skerrey <sethkerrey@gmail.com>
2023-05-25 16:22:33 -0500

    Create UserCard.js

---
+++ src/components/UserCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button } from 'react-bootstrap';
+
+function UserCard({ card }) {
+
+  // Icon card types
+  const getCardIcon = (brand) => {
+    switch (brand.toLowerCase()) {
+      case 'visa':
+        return "fa-brands fa-cc-visa";
+      case 'mastercard':
+        return "fa-brands fa-cc-mastercard";
+      case 'discover':
+        return "fa-brands fa-cc-discover";
+      case 'applepay':
+        return "fa-brands fa-cc-apple-pay";
+      case 'amazonpay':
+        return "fa-brands fa-cc-amazon-pay";
+      case 'paypal':
+        return "fa-brands fa-cc-paypal";
+      default:
+        return "fa-solid fa-credit-card";  // default icon for unknown types
+    }
+  };
+
+  return (
+    <div className="d-inline-flex justify-content-between align-items-center bg-light rounded w-100 mt-1">
+      <FontAwesomeIcon icon={getCardIcon(card.brand)} size="2xl" />
+      <span>ending in {card.last4}</span>
+      <Button variant="light">
+        <FontAwesomeIcon icon="fa-solid fa-trash" size="xs" className="text-danger" />
+      </Button>
+    </div>
+  );
+}
+
+export default UserCard
