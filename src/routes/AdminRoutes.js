81296408cc9f47611f9782465c7f9530f3e8ed07
skerrey <sethkerrey@gmail.com>
2023-06-15 14:36:41 -0500

    Update AdminRoutes.js

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -3,12 +3,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { useFirestore } from '../contexts/FirestoreContext';
 
-export default function PrivateRoutes({ children }) {
+export default function AdminRoutes({ children }) {
   const { currentUser } = useAuth();
+  const { userData } = useFirestore();
   
   return (
-    currentUser ? children : <Navigate to="/login" redirect />
+    currentUser && userData && userData.isAdmin
+    ? children 
+    : currentUser 
+    ? <Navigate to="/" redirect /> // Redirect to home if not an admin
+    : <Navigate to="/login" redirect /> // Redirect to login if user is not logged in OR if not admin
   )
 }
 
