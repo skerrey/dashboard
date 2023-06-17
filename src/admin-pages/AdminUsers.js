700b3c015f7ef706fbce3816bae75387c7b6c15f
skerrey <sethkerrey@gmail.com>
2023-06-17 18:21:27 -0500

    Add function getAllUsers

--- src/admin-pages/AdminUsers.js
+++ src/admin-pages/AdminUsers.js
@@ -1,21 +1,21 @@
-// Description: Account Profile page
-
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card, Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { httpsCallable } from "firebase/functions";
 import { functions } from '../firebase.config';
 
 function AdminUsers() {
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
   const fetchUsers = () => {
-    const getUsers = httpsCallable(functions, 'getUsers');
-    getUsers()
+    const getAllUsers = httpsCallable(functions, 'getAllUsers');
+    getAllUsers()
       .then((result) => {
         console.log(result.data);
         setUsers(result.data);
+        setFilteredUsers(result.data);
       })
       .catch((error) => {
         console.log("whoops something went wrong");
@@ -28,9 +28,19 @@ function AdminUsers() {
     fetchUsers();
   }, []);
 
+
+  useEffect(() => {
+    setFilteredUsers(
+      users.filter(user =>
+        (user.email ? user.email.toLowerCase().includes(searchQuery.toLowerCase()) : false)
+        || (user.displayName ? user.displayName.toLowerCase().includes(searchQuery.toLowerCase()) : false)
+      )
+    );
+  }, [searchQuery, users]);
+  
+
   return (
     <div>
-      
       <div className="page-title">
         Users <FontAwesomeIcon icon="fa-solid fa-user" size="xs" />
       </div>
@@ -38,31 +48,44 @@ function AdminUsers() {
         <Col>
         <Card className="card-account">
           <Card.Body>
-            <Card.Title>Maintenance Requests</Card.Title>
+            <Card.Title>List of Users</Card.Title>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+              />
+              <Button>
+                Search
+              </Button>
+            </Form>
             <div>
-            {users.length > 0 ? (
+            {filteredUsers.length > 0 ? (
               <table className="table table-striped">
                   <thead>
                     <tr>
-                      <th>UID</th>
+                      <th>Name</th>
                       <th>Email</th>
-                      <th>Display Name</th>
                       <th>Email Verified</th>
+                      <th>Created at</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {users.map((user) => (
+                    {filteredUsers.map((user) => (
                       <tr key={user.uid}>
-                        <td>{user.uid}</td>
+                        <td>{user.displayName}</td>                        
                         <td>{user.email}</td>
-                        <td>{user.displayName}</td>
                         <td>{user.emailVerified.toString()}</td>
+                        <td>{user.metadata.creationTime}</td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
             ) : (
-              <li>No users</li>
+              <p className="pt-2 ps-2">No users</p>
             )}
             </div>
           </Card.Body>
