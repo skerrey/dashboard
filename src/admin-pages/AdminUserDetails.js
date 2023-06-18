77a502e90dd4a990b9e9f0317d8153740fa18788
skerrey <sethkerrey@gmail.com>
2023-06-18 10:55:01 -0500

    Update AdminUserDetails.js

--- src/admin-pages/AdminUserDetails.js
+++ src/admin-pages/AdminUserDetails.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFirestore } from '../contexts/FirestoreContext';
 
 function AdminUserDetails() {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams(); // Extract user ID from the route parameters
+  const { getUser } = useFirestore();
+
+  useEffect(() => {
+    console.log(userId);
+    const fetchUser = async () => {
+      try {
+        const userData = await getUser(userId);
+        if (userData) {
+          setUser(userData);
+        } else {
+          console.log(`No user data returned for user id: ${userId}`);
+        }
+      } catch (error) {
+        console.error("Error fetching user data in UserDetail:", error);
+      }
+    };
+
+    fetchUser();
+  }, [userId, getUser]);
+
+
+  if (!user) {
+    return <div>Loading...</div>; 
+  }
+
   return (
-    <div>AdminUserDetails</div>
-  )
+    <div>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
 
-export default AdminUserDetails
+export default AdminUserDetails;
